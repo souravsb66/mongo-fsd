4992ca06554b3514634a860fcf336fa82111b121
Sourav Biswas <sourav.sb56@gmail.com>
2023-10-26 21:10:24 +0530

    code fix

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 const { connection } = require("./db");
 const { userRouter } = require("./routes/user.route");
 const { noteRouter } = require("./routes/notes.route");
+
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
+
+app.get("/", (req,res) => {
+    res.send({"message": "Home page"});
+})
 
 app.listen(process.env.PORT, async () => {
     try {
